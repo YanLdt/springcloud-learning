7412594f438246d11067ea6010de82dabf0002a5
liuyant <liuyant@zbj.com>
2020-07-28 09:04:20 +0800

    init eureka & ribbon

---
+++ user-service/src/main/java/com/yanl/cloud/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package com.yanl.cloud.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket createRestApi(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.yanl.cloud.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    public ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("userService")
+                .description("用户CRUD")
+                .version("1.0")
+                .build();
+    }
+}
---
+++ user-service/src/main/java/com/yanl/cloud/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.yanl.cloud.controller;
+
+import com.yanl.cloud.domain.CommonResult;
+import com.yanl.cloud.domain.User;
+import com.yanl.cloud.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/create")
+    public CommonResult create(@RequestBody User user){
+        userService.create(user);
+        return new CommonResult("操作成功", 200);
+    }
+
+    @GetMapping("/{id}")
+    public CommonResult<User> getUser(@PathVariable Long id){
+        User user = userService.getUser(id);
+        log.info("根据id获取用户信息，用户名称为: {}", user.getUsername());
+        return new CommonResult<>(user);
+    }
+
+    @GetMapping("/getUserByIds")
+    public CommonResult<List<User>> getUserByIds(@RequestParam List<Long> ids){
+        List<User> findUserList = userService.getUserByIds(ids);
+        log.info("根据ids获取用户组，用户组为：{}", findUserList);
+        return new CommonResult<>(findUserList);
+    }
+
+    @GetMapping("/getUserByName")
+    public CommonResult<User> getUserByName(@RequestParam String name){
+        User user = userService.getByUserName(name);
+        log.info("根据用户名称获取用户，用户信息为：{}", user);
+        return new CommonResult<>(user);
+    }
+
+    @PostMapping("/update")
+    public CommonResult update(@RequestBody User user){
+        userService.update(user);
+        return new CommonResult("操作成功", 200);
+    }
+
+    @PostMapping("/delete/{id}")
+    public CommonResult delete(@PathVariable Long id){
+        userService.delete(id);
+        return new CommonResult("操作成功", 200);
+    }
+
+}
